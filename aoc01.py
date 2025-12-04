224e4a170906b8e0c16e667d362ad78f96d31992
Jürgen Strobel <juergen+github@strobel.info>
2025-12-04 01:25:03 +0100

    add day 1

---
+++ aoc01.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+import numpy as np
+from aoc_util import run_aoc
+
+
+def parse_line(line):
+    return int(line[1:]) * (-1) ** (line[0] == "L")
+
+
+def aoc01(rotations):
+    positions = np.add.accumulate([50, *rotations])
+    zeros = np.logical_not(positions % 100)
+    yield np.sum(zeros)
+
+    revolution_r = positions // 100
+    right_pass_zero = np.maximum(revolution_r[1:] - revolution_r[:-1], 0)
+    revolution_l = (positions - 1) // 100
+    left_pass_zero = np.maximum(revolution_l[:-1] - revolution_l[1:], 0)
+    yield np.sum(right_pass_zero) + np.sum(left_pass_zero)
+
+
+if __name__ == "__main__":
+    run_aoc(
+        aoc01,
+        split="lines",
+        apply=parse_line,
+        np_printoptions=dict(linewidth=120, threshold=100, edgeitems=10),
+    )
