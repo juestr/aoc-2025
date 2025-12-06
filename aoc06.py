e6b51c4ebed00ac008ae38ec29315a0cc5983fab
Jürgen Strobel <juergen+github@strobel.info>
2025-12-07 00:51:42 +0100

    add day 6

---
+++ aoc06.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+
+from itertools import islice, zip_longest
+
+import numpy as np
+from funcy import partition_by
+
+from aoc_util import run_aoc
+
+
+def setup(input):
+    *lines, ops = input.splitlines()
+    return lines, ops.split()
+
+
+def aoc06(lines, ops):
+    xs = np.loadtxt(lines, dtype=int)
+    is_add = np.equal(ops, "+")
+    yield np.sum(xs[:, is_add]) + np.sum(np.prod(xs[:, ~is_add], axis=0))
+
+    chars_t = zip_longest(*(iter(line) for line in lines), fillvalue="")
+    lines_t = ("".join(line).strip() for line in chars_t)
+    groups_t = islice(partition_by(bool, lines_t), 0, None, 2)
+    yield sum(
+        (np.sum if add else np.prod)(np.loadtxt(group, dtype=int))
+        for group, add in zip(groups_t, is_add, strict=True)
+    )
+
+
+if __name__ == "__main__":
+    run_aoc(aoc06, transform=setup)
