4d387e96f2d6ea582c4a3d9d4f22710b34e750d3
Jürgen Strobel <juergen+github@strobel.info>
2025-12-08 01:56:16 +0100

    add day 7

---
+++ aoc07.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+import numpy as np
+
+from aoc_util import np_raw_table, run_aoc
+
+
+def aoc07(grid):
+    splitters = grid[1:]
+    beams = grid.astype(int)
+    beams[1:] = 0
+
+    for r in range(1, beams.shape[0]):
+        prev = r - 1
+        split_beams = beams[prev] * splitters[prev]
+        beams[r] = beams[prev] * ~splitters[prev]
+        beams[r][:-1] += split_beams[1:]
+        beams[r][1:] += split_beams[:-1]
+
+    yield np.sum(np.logical_and(splitters, beams[:-1]))
+    yield np.sum(beams[-1])
+
+
+if __name__ == "__main__":
+    run_aoc(
+        aoc07,
+        apply=(np_raw_table, dict(offs=ord("."), dtype=bool)),
+        np_printoptions=dict(linewidth=120, threshold=100, edgeitems=10),
+    )
