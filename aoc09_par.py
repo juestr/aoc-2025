ee680372d4a8fee6b82581a45a205fbed470fba2
Jürgen Strobel <juergen+github@strobel.info>
2025-12-10 13:03:32 +0100

    add threaded version of day 9
    - numpy seems to have stopped using openmp, threading?

--- aoc09_par.py
+++ aoc09_par.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 
+import concurrent.futures as fut
+
 import numpy as np
 
 from aoc_util import run_aoc
@@ -18,11 +20,21 @@ def aoc09(tiles: np.typing.NDArray[int]):
     edges = np.column_stack([tiles, np.roll(tiles, -1, axis=0)])
     edges[:, 0::2].sort(axis=1)
     edges[:, 1::2].sort(axis=1)
+
     # cross table whether edge is wholly <left, above, right, below> of rect
-    rects_x_edges = edges[None, :, 2] <= rects[:, None, 0]
-    rects_x_edges |= edges[None, :, 3] <= rects[:, None, 1]
-    rects_x_edges |= edges[None, :, 0] >= rects[:, None, 2]
-    rects_x_edges |= edges[None, :, 1] >= rects[:, None, 3]
+    with fut.ThreadPoolExecutor(max_workers=4) as pool:
+        ready = fut.as_completed(
+            [
+                pool.submit(np.greater_equal.outer, rects[:, 0], edges[:, 2]),
+                pool.submit(np.greater_equal.outer, rects[:, 1], edges[:, 3]),
+                pool.submit(np.less_equal.outer, rects[:, 2], edges[:, 0]),
+                pool.submit(np.less_equal.outer, rects[:, 3], edges[:, 1]),
+            ]
+        )
+        rects_x_edges = next(ready).result()
+        rects_x_edges |= next(ready).result()
+        rects_x_edges |= next(ready).result()
+        rects_x_edges |= next(ready).result()
     valid = np.all(rects_x_edges, axis=1)
     yield np.max(areas[valid])
 
