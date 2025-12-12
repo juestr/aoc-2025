5a66f1b5e82892dea39dc8a9b24372c78d6fc2ba
Jürgen Strobel <juergen+github@strobel.info>
2025-12-12 09:17:21 +0100

    add day 12

--- aoc11.py
+++ aoc11.py
@@ -20,6 +20,7 @@ def create_graph(input):
 def aoc11(g: nx.DiGraph) -> AOC:
     def count_paths(g, s, t):
         if s in g and t in g:
+            # note g is a DAG; enumerating simple_paths would be way too slow
             subg = g.subgraph(nx.descendants(g, s) & nx.ancestors(g, t) | {s, t})
             nodes = nx.topological_sort(subg)
             npaths = {next(nodes): 1}
@@ -33,8 +34,11 @@ def aoc11(g: nx.DiGraph) -> AOC:
         if any(n not in g for n in waypoints):
             return 0
         else:
+            # again g is a DAG, there can be only one order the middle nodes
+            # are traversed
             if not nx.has_path(g, *waypoints[1:3]):
                 waypoints[1:3] = waypoints[1:3][::-1]
+            # and absent cycles, the legs are independent
             legs = (count_paths(g, n1, n2) for n1, n2 in pairwise(waypoints))
             return prod(legs)
 
---
+++ aoc12.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+
+import re
+
+import numpy as np
+from funcy import autocurry, lmap, map, re_iter
+
+from aoc_util import AOC, np_raw_table, run_aoc
+
+
+def setup(input):
+    return (
+        np.array(
+            lmap(
+                autocurry(np_raw_table)(cmp=ord("#")),
+                re_iter(r"^\d:\n(...\n...\n...\n)", input, re.M),
+            )
+        ),
+        np.array(
+            lmap(
+                autocurry(lmap)(int),
+                re_iter(
+                    r"^(\d+)x(\d+): (\d+) (\d+) (\d+) (\d+) (\d+) (\d+)", input, re.M
+                ),
+            )
+        ),
+    )
+
+
+def aoc12(shapes: np.typing.NDArray[np.uint8], regions: np.typing.NDArray[int]) -> AOC:
+    def check_region(region):
+        shape, todo = region[:2], region[2:]
+        if np.sum(todo) * 9 <= np.prod(shape // 3 * 3):
+            return True
+        elif todo @ weights > np.prod(shape):
+            return False
+        else:
+            assert False, "unclear case, this would require a lot of work"
+
+    weights = np.sum(shapes, axis=(1, 2))
+    yield sum(map(check_region, regions))
+
+
+if __name__ == "__main__":
+    run_aoc(aoc12, transform=setup)
